508a9c8da360c4b02ea40aa3ede2309b5dbeb30e
Harish-3432 <tanwerh460@gmail.com>
2024-01-08 10:55:20 +0530

    08

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
@@ -35,5 +36,7 @@ const videoSchema = new Schema(
         ref:"User",
     }
 },{timestamps:true});
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video",videoSchema)
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -2,7 +2,7 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message="Something went wrong",
-        error = [],
+        errors = [],
         stack = ""
     ){
         super(message)
@@ -10,7 +10,7 @@ class ApiError extends Error {
         this.data = null
         this.message = message
         this.success = false
-        this.errors = this.errors 
+        this.errors = errors 
 
         if(stack){
             this.stack = stack
